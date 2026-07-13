ca2800ab75eff2bea6ae328e5cf1738252ea5b92
IB BOTS <msanodedata@gmail.com>
2026-07-13 12:04:34 +0530

    Update bot3.py

--- BOTS/bot3.py
+++ BOTS/bot3.py
@@ -34,7 +34,6 @@ import html as _html
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 
-
 # ==========================================
 # ENTERPRISE CONFIGURATION
 # ==========================================
